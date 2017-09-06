e3053aba2522b108c297bcd0f10e3a12077fd395
Mark Reynolds <mark.reynolds@digital.hmrc.gov.uk>
2017-09-06 13:22:28 +0100

    Chapter 8

---
+++ datatype.hs
@@ -0,0 +1,9 @@
+type Pos = (Int, Int)
+
+data Move = North | South | East | West
+
+move :: Move -> Pos -> Pos
+move North (x,y) = (x, y+1)
+move South (x,y) = (x, y-1)
+move East (x,y) = (x+1, y)
+move West (x,y) = (x-1, y)
