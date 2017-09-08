6937999792230286373d23614908edf593e115b4
Mark Reynolds <mark.reynolds@digital.hmrc.gov.uk>
2017-09-08 12:16:04 +0100

    More chapter8

--- datatype.hs
+++ datatype.hs
@@ -1,9 +1,58 @@
 type Pos = (Int, Int)
 
-data Move = North | South | East | West
+data Move = North | South | East | West deriving Show
 
 move :: Move -> Pos -> Pos
 move North (x,y) = (x, y+1)
 move South (x,y) = (x, y-1)
 move East (x,y) = (x+1, y)
 move West (x,y) = (x-1, y)
+
+moves :: [Move] -> Pos -> Pos
+moves [] p = p
+moves (m:ms) p = moves ms (move m p)
+
+rev :: Move -> Move
+rev North = South
+rev South = North
+rev East = West
+rev West = East
+
+createPos :: Int -> Int -> Pos
+createPos x y = (x, y)
+
+
+data Shape = Circle Float | Rect Float Float
+
+square :: Float -> Shape
+square n = Rect n n
+
+area :: Shape -> Float
+area (Circle r) = pi * r^2
+area (Rect x y) = x * y
+
+data List' a = Nil | Cons a (List' a)
+
+lenl :: List' a -> Int
+lenl Nil = 0
+lenl (Cons _ xs) = 1 + lenl xs
+
+data Tree a = Leaf a |  Node (Tree a) a (Tree a) deriving Show
+
+t :: Tree Int
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+
+
+occurs :: Eq a => a -> Tree a -> Bool
+occurs x (Leaf y) = x == y
+occurs x (Node l y r) = x == y || occurs x l || occurs x r
+
+flatten :: Tree a -> [a]
+flatten (Leaf x) = [x]
+flatten (Node l x r) = flatten l ++ [x] ++ flatten r
+
+occurs' :: Ord a => a -> Tree a -> Bool
+occurs' x (Leaf y) = x == y
+occurs' x (Node l y r) | x == y = True
+                       | x < y = occurs' x l
+                       | otherwise = occurs' x r
