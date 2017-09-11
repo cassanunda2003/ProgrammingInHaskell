5ca9a0c8a5575a0841cc182527f34750c2a28593
Mark Reynolds <mark.reynolds@digital.hmrc.gov.uk>
2017-09-11 12:01:23 +0100

    Optimisations

--- countdown.hs
+++ countdown.hs
@@ -1,3 +1,6 @@
+main :: IO ()
+main = print (solutions [1,3,7,10,25,50] 831)
+
 data Op = Add | Sub | Mul | Div
 
 instance Show Op where
@@ -76,4 +79,21 @@ ops :: [Op]
 ops = [Add, Sub, Mul, Div]
 
 solutions :: [Int] -> Int -> [Expr]
-solutions ns n = [e | ns' <- choices ns, e<- exprs ns', eval e == [n]]
+solutions ns n = [e | ns' <- choices ns, e <- exprs ns', eval e == [n]]
+
+type Result = (Expr, Int)
+
+results :: [Int] -> [Result]
+results [] = []
+results [n] = [(Val n,n)|n > 0]
+results ns = [res | (ls, rs) <- split ns,
+                    lx <- results ls,
+                    ry <- results rs,
+                    res <- combine' lx ry]
+
+combine' :: Result -> Result -> [Result]
+combine' (l, x) (r, y) = [(App o l r, apply o x y)| o <- ops, valid o x y]
+
+
+solutions' :: [Int] -> Int -> [Expr]
+solutions' ns n = [e | ns' choices ns, (e,m) <- results ns', m == n]
